3533d0775c21ca0e6dc0c512f93d31934d5e8f02
Hazumiran <diazazhari32@gmail.com>
2022-05-19 10:05:21 +0700

    terbaru

--- BT_driver.h
+++ BT_driver.h
@@ -15,16 +15,17 @@ void printTree(node* root);
 void DelTree(struct node* root_node);
 struct node* newNode(int value);
 struct node* insertNode(struct node* srcnode, int value);
-void printArray(int arr[], int n);
+void printArray(int *arr, int n);
 void heapifyAscending(int arr[], int n, int i) ;
 void heapSortAscending(int arr[], int n);
 void heapSortDesending(int arr[], int n);
 void heapifyDesending(int arr[], int n, int i);
 void swap(int *a, int *b);
-void DeleteArray(int arr[],int data,int count);
+void DeleteArray(int *arr,int data,int count);
 
 struct node* deleteNode(struct node* root, int key);
 struct node* minValueNode(struct node* node);
 void setcolor(unsigned short color);
+bool checkNode(struct node *root, int key);
 
 #endif
--- menu.cpp
+++ menu.cpp
@@ -14,6 +14,7 @@ void menu_list(){
 	cout << "2. Delete Tree\n";
 	cout << "3. Delete Node\n";
 	cout << "4. HeapSort\n";
+	cout << "6. Check Nilai Tree\n";
 	cout << "\n\n";
 	cout << "Tree Data : \n\n";
 	
@@ -24,8 +25,9 @@ void menu_utama(){
 	int data = 0;
 	int inp;
 	int count = 0; //jumlah total array
-	int arr[3]={5,8,9};		//array
-
+	int *arr;		//array
+	arr = (int *) malloc(count);
+	
 	BackMenu:
 	menu_list();
 	printTree(root_node);
@@ -39,18 +41,20 @@ void menu_utama(){
 			  setcolor(2);
 			  printf("MAX HEAP SORT : ");
 			  printArray(arr, count);
-			  setcolor(7);
-			  
+			  setcolor(7);			  
 	cout << "\n\nYour Choice : ";		
 	scanf("%d",&inp);
 //	fflush(stdin);
  	switch(inp){
  		case 1:
+ 			printf("%d",count);
  			while(data != 100){
 			cout << "Data : ";
 			cin >> data ;
-			arr[count] = data;			
 			count++;
+			arr = (int *) realloc(arr, count);
+			arr[count-1] = data;			
+			
 			if(root_node == NULL){
 				root_node = insertNode(root_node, data);
 			}else{
@@ -81,16 +85,6 @@ void menu_utama(){
 			setcolor(7);	
 			goto BackMenu;
 			break;
-		case 5:
-			  heapSortDesending(arr, count);
-			  setcolor(2);
-			  printf("MAX HEAP SORT : ");
-			  printArray(arr, count);
-			  setcolor(7);
-			  system("pause");
-			  system("cls");
-			  goto BackMenu;
-			  break;
 		case 4:
 			  heapSortAscending(arr, count);
 			  setcolor(2);
@@ -101,12 +95,39 @@ void menu_utama(){
 			  system("cls");
 			  goto BackMenu;
 			  break;
+		case 5:
+			  heapSortDesending(arr, count);
+			  setcolor(2);
+			  printf("MAX HEAP SORT : ");
+			  printArray(arr, count);
+			  setcolor(7);
+			  system("pause");
+			  system("cls");
+			  goto BackMenu;
+			  break;
+		case 6:
+		cout << "Data : ";
+			cin >> data;
+			if(checkNode(root_node, data)){
+				setcolor(2);
+				cout << "Data ada di dalam Tree\n";
+				setcolor(7);
+			}else{
+				setcolor(4);
+				cout << "Data tidak ada di dalam Tree\n";
+				setcolor(7);
+			}
+			system("pause");
+			system("cls");
+			goto BackMenu;
+			break;
+		
 		default:
 			system("CLS");
 			setcolor(4);
 			cout << "Invalid Input, Please Retry...\n";
 			setcolor(7);
-//			fflush(stdin);
+			fflush(stdin);
 			goto BackMenu;	
 	 }
 }
